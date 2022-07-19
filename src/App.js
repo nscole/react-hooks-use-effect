3b7414e4f85b94a7bf24b17aaf8a667157b0c048
Naomi Cole <naomisarahcole@gmail.com>
2022-07-19 20:11:58 +1000

    started page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import React, { useState, useEffect } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState("");
   useEffect(() => {
     console.log("useEffect called");
   });
 
+  function UpdateCounter(){
+    setCount(count + 1);
+  }
+
+  function UpdateText(obj){
+    setText(obj.target.value)
+  }
+
   console.log("Component rendering");
 
-  return <button>Click Me</button>;
+  return <div>
+    <button onClick={UpdateCounter}>I've been clicked {count} times.</button>
+    <input type="text" placeholder="type away"
+    value={text} 
+    onChange={UpdateText}/> 
+
+    </div>
 }
 
 export default App;
